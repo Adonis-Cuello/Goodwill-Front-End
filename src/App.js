cc3273f52d4491c6121852faf0bf96781e534486
Adonis-Cuello <adonisac2002@gmail.com>
2021-11-16 10:41:27 -0500

    form unfinished

---
+++ src/App.js
@@ -0,0 +1,161 @@
+import './App.css';
+import Form from 'react-bootstrap/Form';
+import Table from 'react-bootstrap/Table'
+import React, { useEffect, useState } from "react";
+import axios, {get, post} from 'axios';
+
+
+
+
+
+function App() {
+    return (
+    <><><Form>
+      <Form.Group className="mb-3" controlId="formDate">
+        <Form.Label>Date</Form.Label>
+        <Form.Control type="date" placeholder="Enter date" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formName">
+        <Form.Label>Full Name</Form.Label>
+        <Form.Control type="name" placeholder="Full Name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formAddress">
+        <Form.Label>address</Form.Label>
+        <Form.Control type="address" placeholder="Enter Address" />
+      </Form.Group>
+    </Form>
+      <Table striped bordered hover size="sm">
+      </Table></>
+
+      
+      
+    
+    
+      <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" /><table class="table table-bordered table-hover">
+        <thead>
+          <tr>
+            <th class="col-md-4">Description:</th>
+            <th class="col-md-4">Donor notes for tax records:</th>
+            <th class="col-md-2">Boxes</th>
+            <th class="col-md-2">Bags</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr ng-repeat="name in getdrugnameNewArray">
+          <td>
+                  <select class="form-control">
+                    <option></option>
+                    <option>Clothing</option>
+                    <option>Household</option>
+                    <option>Furniture</option>
+                    <option>Collectables/Antiques</option>
+                    <option>Electronics</option>
+                    <option>Other</option>
+                  </select>
+                </td>
+            <td><input type="text" placeholder="Brief summary"></input></td>
+            <td><input type="text" placeholder="# of boxes"></input></td>
+            <td><input type="text" placeholder="# of bags"></input></td>
+          </tr>
+          <tr ng-repeat="name in getdrugnameNewArray">
+          <td>
+                  <select class="form-control">
+                  <option></option>
+                    <option>Clothing</option>
+                    <option>Household</option>
+                    <option>Furniture</option>
+                    <option>Collectables/Antiques</option>
+                    <option>Electronics</option>
+                    <option>Other</option>
+                  </select></td>
+            <td><input type="text" placeholder="Brief summary"></input></td>
+            <td><input type="text" placeholder="# of boxes"></input></td>
+            <td><input type="text" placeholder="# of bags"></input></td>
+            </tr>
+            <tr ng-repeat="name in getdrugnameNewArray">
+          <td>
+                  <select class="form-control">
+                  <option></option>
+                    <option>Clothing</option>
+                    <option>Household</option>
+                    <option>Furniture</option>
+                    <option>Collectables/Antiques</option>
+                    <option>Electronics</option>
+                    <option>Other</option>
+                  </select>
+                </td>
+            <td><input type="text" placeholder="Brief summary"></input></td>
+            <td><input type="text" placeholder="# of boxes"></input></td>
+            <td><input type="text" placeholder="# of bags"></input></td>
+          </tr>
+          <tr ng-repeat="name in getdrugnameNewArray">
+          <td>
+                  <select class="form-control">
+                  <option></option>
+                    <option>Clothing</option>
+                    <option>Household</option>
+                    <option>Furniture</option>
+                    <option>Collectables/Antiques</option>
+                    <option>Electronics</option>
+                    <option>Other</option>
+                  </select>
+                </td>
+            <td><input type="text" placeholder="Brief summary"></input></td>
+            <td><input type="text" placeholder="# of boxes"></input></td>
+            <td><input type="text" placeholder="# of bags"></input></td>
+          </tr>
+          <tr ng-repeat="name in getdrugnameNewArray">
+          <td>
+                  <select class="form-control">
+                  <option></option>
+                    <option>Clothing</option>
+                    <option>Household</option>
+                    <option>Furniture</option>
+                    <option>Collectables/Antiques</option>
+                    <option>Electronics</option>
+                    <option>Other</option>
+                  </select>
+                </td>
+            <td><input type="text" placeholder="Brief summary"></input></td>
+            <td><input type="text" placeholder="# of boxes"></input></td>
+            <td><input type="text" placeholder="# of bags"></input></td>
+          </tr>
+          <tr ng-repeat="name in getdrugnameNewArray">
+          <td>
+                  <select class="form-control">
+                  <option></option>
+                    <option>Clothing</option>
+                    <option>Household</option>
+                    <option>Furniture</option>
+                    <option>Collectables/Antiques</option>
+                    <option>Electronics</option>
+                    <option>Other</option>
+                  </select>
+                </td>
+            <td><input type="text" placeholder="Brief summary"></input></td>
+            <td><input type="text" placeholder="# of boxes"></input></td>
+            <td><input type="text" placeholder="# of bags"></input></td>
+          </tr>
+          <tr ng-repeat="name in getdrugnameNewArray">
+          <td>
+                  <select class="form-control">
+                  <option></option>
+                    <option>Clothing</option>
+                    <option>Household</option>
+                    <option>Furniture</option>
+                    <option>Collectables/Antiques</option>
+                    <option>Electronics</option>
+                    <option>Other</option>
+                  </select>
+                </td>
+            <td><input type="text" placeholder="Brief summary"></input></td>
+            <td><input type="text" placeholder="# of boxes"></input></td>
+            <td><input type="text" placeholder="# of bags"></input></td>
+          </tr>
+        </tbody>
+      </table></>
+
+      
+)};
+
+export default App;
---
+++ src/components/FormClient.js
@@ -0,0 +1,28 @@
+// import React, { useState } from 'react';
+// import axios, {get, post} from 'axios';
+// import Form from 'react-bootstrap/Form';
+
+// import './App.css';
+
+// // export default function FormClient(){
+// //     const [body, setBody] = useState('not Set');
+// //     const onButtonCLick = (event)=>{
+// //         console.log('button click');
+// //         get('http://localhost:8080')
+// //         .then (response => {
+// //             console.log(response);
+// //             setBody(response.data);
+// //         })
+//     // }
+//     return (
+//         <Form>
+//   <Form.Group className="mb-3" controlId="formGroupEmail">
+//     <Form.Label>Email address</Form.Label>
+//     <Form.Control type="email" placeholder="Enter email" />
+//   </Form.Group>
+//   <Form.Group className="mb-3" controlId="formGroupPassword">
+//     <Form.Label>Password</Form.Label>
+//     <Form.Control type="password" placeholder="Password" />
+//   </Form.Group>
+// </Form>
+// )};
